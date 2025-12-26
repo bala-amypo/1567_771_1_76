b1e154099ee5f774fda8728727d947b4d4dc3b96
bala-amypo <bala@amypo.in>
2025-12-26 12:51:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+
+         String secret =
+                "THIS_IS_A_PRODUCTION_SECRET_KEY_32_CHARS_MINIMUM";
+
+        long expirationMs = 1000 * 60 * 60; // 1 hour
+
+        return new JwtTokenProvider(secret, expirationMs);
+    }
+}
