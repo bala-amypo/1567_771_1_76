f9d3b9afbd5618cdf7f09e88df725e997c045e11
bala-amypo <bala@amypo.in>
2025-12-28 08:25:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/ConflictCaseRepository.java
+++ src/main/java/com/example/demo/repository/ConflictCaseRepository.java
@@ -2,12 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ConflictCase;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface ConflictCaseRepository extends JpaRepository<ConflictCase, Long> {
+public interface ConflictCaseRepository
+        extends JpaRepository<ConflictCase, Long> {
 
-    List<ConflictCase> findByPrimaryPersonIdOrSecondaryPersonId(Long primaryId, Long secondaryId);
+    List<ConflictCase> findByPrimaryPersonIdOrSecondaryPersonId(
+            Long primaryPersonId,
+            Long secondaryPersonId
+    );
 }
